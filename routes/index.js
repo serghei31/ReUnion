bc93da6cbe1e743f14483f32edff776cc28f6f74
Serghei <sergheic.89@gmail.com>
2023-02-15 11:20:32 +0200

    Added MongoDB connection

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-   res.send('Hello World!!!');
+   res.status(200).send('Hello World!!!');
 });
 
 module.exports = router;
